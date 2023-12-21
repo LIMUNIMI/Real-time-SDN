6104eaf946dd3cb2a181a9b857cfb150b05c2c67
MarcoFontana <marco.fontana95@hotmail.com>
2023-12-21 16:04:37 +0100

    wall clipping fix

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -244,18 +244,18 @@ void RealtimeSDNAudioProcessor::lookAtSource()
 void RealtimeSDNAudioProcessor::parameterChanged(const String& paramID, float newValue)
 {
     if (paramID == "SourceX")
-        room.setSourcePos(juce::jlimit(0.000001f, 0.9999999f, newValue), 'x');
+        room.setSourcePos(juce::jlimit(0.0001f, 0.9999f, newValue), 'x');
     if (paramID == "SourceY")
-        room.setSourcePos(juce::jlimit(0.000001f, 0.9999999f, newValue), 'y');
+        room.setSourcePos(juce::jlimit(0.0001f, 0.9999f, newValue), 'y');
     if (paramID == "SourceZ")
-        room.setSourcePos(juce::jlimit(0.000001f, 0.9999999f, newValue), 'z');
+        room.setSourcePos(juce::jlimit(0.0001f, 0.9999f, newValue), 'z');
 
     if (paramID == "ListenerX")
-        room.setListenerPos(juce::jlimit(0.000001f, 0.9999999f, newValue), 'x');
+        room.setListenerPos(juce::jlimit(0.0001f, 0.9999f, newValue), 'x');
     if (paramID == "ListenerY")
-        room.setListenerPos(juce::jlimit(0.000001f, 0.9999999f, newValue), 'y');
+        room.setListenerPos(juce::jlimit(0.0001f, 0.9999f, newValue), 'y');
     if (paramID == "ListenerZ")
-        room.setListenerPos(juce::jlimit(0.000001f, 0.9999999f, newValue), 'z');
+        room.setListenerPos(juce::jlimit(0.0001f, 0.9999f, newValue), 'z');
 
     if (paramID == "OutputMode")
     {
